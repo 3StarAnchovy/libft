65550dc5276b70a3a6cd315907cab0426d922bb3
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-11-13 23:13:08 +0900

    add strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 21:41:11 by jihong            #+#    #+#             */
+/*   Updated: 2021/11/13 23:12:35 by jihong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	while (len != 0)
+	{
+		if (*(str + len) == c)
+			return ((char *)(str + len));
+		len --;
+	}
+	return (NULL);
+}
