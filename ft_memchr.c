1b07037ec44567e0ab772492df4ac87dfbc80167
Jimin Hong <jihong@c7r5s2.42seoul.kr>
2021-11-16 16:13:07 +0900

    fix memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,11 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:49:18 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/16 16:10:36 by jihong           ###   ########.fr       */
+/*   Updated: 2021/11/16 16:12:44 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -27,20 +24,4 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		i ++;
 	}
 	return (NULL);
-}
-
-void printMemory(void *ptr)
-{
-	if(ptr == NULL)
-		printf("null");
-	else
-		printf("%p",ptr);
-}
-
-int	main(void)
-{
-	char a[10] = "abcdef";
-	printMemory(memchr(a,'c',3));
-	printf("\n");
-	printMemory(ft_memchr(a,'c',3));
 }
