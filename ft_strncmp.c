caf388e46e24b041c24e7405540becc26edbf1d6
Jimin Hong <jihong@c7r5s2.42seoul.kr>
2021-11-16 15:36:21 +0900

    add strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/10 14:57:21 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/12 19:52:55 by jihong           ###   ########.fr       */
+/*   Created: 2021/11/16 15:21:18 by jihong            #+#    #+#             */
+/*   Updated: 2021/11/16 15:36:01 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
-int	ft_isprint(int c);
-int	ft_isdigit(int c);
-int	ft_isascii(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-size_t	ft_strlen(const char *c);
-size_t	ft_strlcpy(const void *dest, const void *src, size_t size);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(s1 + i) - *(s2 + i) > 0)
+			return (*(s1 + i) - *(s2 + i));
+		else if (*(s1 + i) - *(s2 + i) < 0)
+			return (*(s1 + i) - *(s2 + i));
+		i ++;
+	}
+	return (0);
+}
