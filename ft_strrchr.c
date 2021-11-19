03e5308f2ca40986626f97f8232ca665b9bdab1e
Jimin Hong <jihong@c5r5s5.42seoul.kr>
2021-11-19 18:48:13 +0900

    fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 21:41:11 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/16 14:04:58 by jihong           ###   ########.fr       */
+/*   Updated: 2021/11/19 18:47:10 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *str, int c)
 	int	len;
 
 	len = ft_strlen(str);
-	while (len != 0)
+	while (len != -1)
 	{
 		if (*(str + len) == c)
 			return ((char *)(str + len));
