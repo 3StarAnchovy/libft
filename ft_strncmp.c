84502293eac6f86c0f8041409e0c3431f8210db7
Jimin Hong <jihong@c5r5s5.42seoul.kr>
2021-11-19 19:40:28 +0900

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:21:18 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/16 15:36:01 by jihong           ###   ########.fr       */
+/*   Updated: 2021/11/19 19:39:44 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,21 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n)
 	{
 		if (*(s1 + i) - *(s2 + i) > 0)
-			return (*(s1 + i) - *(s2 + i));
+			return ((unsigned char)*(s1 + i) - (unsigned char)*(s2 + i));
 		else if (*(s1 + i) - *(s2 + i) < 0)
-			return (*(s1 + i) - *(s2 + i));
+			return ((unsigned char)*(s1 + i) - (unsigned char)*(s2 + i));
 		i ++;
 	}
 	return (0);
 }
+
+// #include<stdio.h>
+// #include<string.h>
+// int main(void)
+// {
+// 	printf("%d",ft_strncmp("test\200","test\0",6));
+// 	printf("\n");
+// 	printf("%d",strncmp("test\200","test\0",6));
+// 	printf("\n");
+// 	printf("%d", ft_strncmp("\2","\0",2));
+// }
