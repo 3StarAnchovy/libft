fda5b29e6ccf36e1f3bde9cd38300322a1775bd7
Jimin Hong <jihong@c6r8s5.42seoul.kr>
2021-11-22 19:03:15 +0900

    fix putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 00:12:22 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/22 19:00:50 by jihong           ###   ########.fr       */
+/*   Updated: 2021/11/22 19:03:01 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ static void	rec_nb(int div, int mod, char str, int fd)
 	write(fd, &str, 1);
 }
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	char str;
+	char	str;
 
 	if (fd < 0)
 		return ;
 	if (n == -2147483648)
-		write(fd,"-2147483648",11);
-	else if(n < 0)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
 	{
 		write(fd, "-", 1);
 		n *= -1;
