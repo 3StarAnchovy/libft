d662344ffddd5d98abb108e159e5c6174d41d196
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-12-02 15:33:10 +0900

    asdf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 19:40:47 by jihong            #+#    #+#             */
-/*   Updated: 2021/11/30 17:17:25 by jihong           ###   ########.fr       */
+/*   Updated: 2021/12/01 18:04:59 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -204,22 +204,22 @@
 // }
 
 //split
-int main(void)
-{
-	char *a = "hello,fxxking,world,,!,";
-	//char *b = "abcde";
-	char **c = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
-	for(int i = 0; i < 10; i ++)
-		printf("%s\n",c[0]);
-	free(c);
-	//char *c = ft_strndup(b + 1,2);
-	//printf("%s",c);å
-	//kadjsfklajsdfkl;j안해 ^^ㅣ바
-}
-
-//strtrim
 // int main(void)
 // {
-// 	printf("%s\n",ft_strtrim("abqbc","abc"));
-// 	printf("%s",ft_strchr("abc",'b'));
+// 	char *a = "hello,fxxking,world,,!,";
+// 	//char *b = "abcde";
+// 	char **c = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
+// 	for(int i = 0; i < 10; i ++)
+// 		printf("%s\n",c[0]);
+// 	free(c);
+// 	//char *c = ft_strndup(b + 1,2);
+// 	//printf("%s",c);å
+// 	//kadjsfklajsdfkl;j안해 ^^ㅣ바
 // }
+
+//strtrim
+int main(void)
+{
+	printf("%s\n",ft_strtrim("\t   \n\n\n  \n\n\t    Hello \t  Please\n Trim me !\t\t\t\n  \t\t\t\t  ", " \n\t"));
+	//printf("%s",ft_strchr("abc",'b'));
+}
