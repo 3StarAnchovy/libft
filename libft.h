68fbbd5afb2ebdae1407d8bd86a9bcabec6166b0
Jimin Hong <jihong@c5r6s5.42seoul.kr>
2021-12-02 17:40:15 +0900

    fix strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 14:57:21 by jihong            #+#    #+#             */
-/*   Updated: 2021/12/02 16:45:26 by jihong           ###   ########.fr       */
+/*   Updated: 2021/12/02 17:35:15 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef struct	s_list
 {
 	void 			*content;
 	struct s_list 	*next;
-}					t_list;
+}t_list;
 
 int		ft_isprint(int c);
 int		ft_isdigit(int c);
@@ -29,7 +29,7 @@ int		ft_toupper(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void 	*ft_memmove(void *dest, const void *src, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t size);
 void	*ft_memset(void *ptr, int value, size_t num);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
